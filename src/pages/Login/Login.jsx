a60a1749094aa9d5446fd33aac41e4d2a8b78eec
Thejellyfish1024 <alamin102410@gmail.com>
2023-11-25 11:29:30 +0600

    created navbar conmponent

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,19 @@
-
+import logo from '../../assets/elite-care-logo.png'
 
 const Login = () => {
     return (
-        <div>
-            LOgin
+        <div className=' min-h-screen bg-[#f0ead2]'>
+            <div className="flex max-w-7xl mx-auto">
+                <div className="w-1/2">
+                    <img src="https://www.froedtert.com/sites/default/files/styles/one_column/public/image/2018-09/be-an-engaged-health-care-consumer.jpg" alt="" />
+                </div>
+                <div>
+                    <div className='flex gap-3 items-center'>
+                        <img src={logo} className='w-12 h-12 rounded-full' alt="" />
+                        <h2 className='text-2xl font-bold italic'>Elite Care</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,9 +1,34 @@
-
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../../assets/elite-care-logo.png'
+import { Button } from '@mui/material';
 
 const NavBar = () => {
+    const navLinks = <>
+        <ul className="  text-white flex gap-5">
+            <li><NavLink className={({ isActive }) => isActive ? "text-[#48cae4] border-b-2 p-1 border-[#48cae4] font-bold" : "font-bold p-1"}
+                to='/'>Home</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? "text-[#48cae4] border-b-2 p-1 border-[#48cae4]  font-bold" : " p-1"}
+                to='/available-camps'>Available Camps</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? "text-[#48cae4] border-b-2 p-1 border-[#48cae4] font-bold" : " p-1"}
+                to='/dashboard'>Dashboard</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? "text-[#48cae4] border-b-2 p-1 border-[#48cae4] font-bold" : " p-1"}
+                to='/contact-us'>Contact Us</NavLink></li>
+        </ul>
+    </>
     return (
-        <div>
-            
+        <div className="bg-[#023047] w-full p-5 ">
+            <div className='max-w-7xl mx-auto flex justify-between text-white items-center font-semibold'>
+                <div className='flex gap-3 items-center'>
+                    <img src={logo} className='w-12 h-12 rounded-full' alt="" />
+                    <h2 className='text-2xl font-bold italic'>Elite Care</h2>
+                </div>
+                <div>
+                    {navLinks}
+                </div>
+                <div>
+                    <Link to='/login'><Button variant="outlined" sx={{color: '#48cae4', borderColor: '#48cae4'}}>Login</Button></Link>
+                </div>
+            </div>
         </div>
     );
 };
