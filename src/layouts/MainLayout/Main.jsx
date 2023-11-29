630eb2aade2ab8747f3ba2dec40ca8918a12e3fd
Thejellyfish1024 <alamin102410@gmail.com>
2023-11-29 10:47:26 +0600

    added footer component

--- src/layouts/MainLayout/Main.jsx
+++ src/layouts/MainLayout/Main.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../../pages/Shared/NavBar/NavBar";
 import { Toaster } from "react-hot-toast";
+import Footer from "../../pages/Shared/Footer/Footer";
 
 
 const Main = () => {
@@ -8,8 +9,9 @@ const Main = () => {
         <div>
             <NavBar></NavBar>
             <div className="pt-[88px]">
-            <Outlet></Outlet>
+                <Outlet></Outlet>
             </div>
+            <Footer></Footer>
             <Toaster />
         </div>
     );
